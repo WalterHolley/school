f99f601317d7ff2428e00841b439790dc415d702
Walter Holley III <holley.walter@gmail.com>
2016-06-17 21:22:14 -0700

    -constants defined -input added

---
+++ CSC110/week1/holleyIIIP1.py
@@ -0,0 +1,31 @@
+'''
+Project 1 - Walter Holley III
+6/17/2016
+Takes input for an order of potted petunias
+ranging from small to large sized pots, and
+outputs the following:
+-number of petunia plants needed
+-total bags of potting soil needed
+-the cost(subtotal, tax, and total price) of the order
+'''
+#constants
+small_pot_diameter = 9
+small_pot_height = 10
+small_pot_price = 9.99
+med_pot_diameter = 12
+med_pot_height = 14
+med_pot_price = 15.99
+large_pot_diameter = 18
+large_pot_height = 20
+large_pot_price = 24.99
+potting_soil_price = 14.79
+potting_soil_bag = 1.8
+
+#input
+order_num = input('Enter order number: ')
+pots_small = input('Enter number of small pots: ')
+pots_med = input('Enter number of medium pots: ')
+pots_large = input('Enter number of large pots: ')
+
+#calculations
+
