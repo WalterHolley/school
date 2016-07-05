a6de0d16d5a0c9cfd0d326ee5e1cd2dd0ae340db
Walter Holley III <holley.walter@gmail.com>
2016-07-05 15:51:19 -0700

    initial commit of project 2

---
+++ CSC110/week2/proj4.py
@@ -0,0 +1,41 @@
+#Project 2 - Walter Holley
+'''
+7/5/2016
+2D & 3D Geometry Calculator
+This application takes in a value and unit
+of measure.  Then, runs that information against
+different geometrical formulas, and prints the results
+'''
+'''
+#Display sphere results report
+def displaySphereResults(value, unit):
+
+#Display circle results report
+def displayCircleResults(value, unit):
+    
+
+#Display cube results report
+def displayCubeResults(value, unit):
+
+
+#Display Square results report
+def displaySquareResults(value, unit):
+'''    
+
+
+
+#get input from user, and display result reports
+def getInput():
+    value = input('Enter size for side and radius: ')
+    unit = input('Enter unit of measure, e.g., inches: ')
+
+    return value, unit
+
+#primary point of execution
+def main():
+    value, unit = getInput()
+    print(value, unit)
+
+
+##MAIN EXECUTION##
+main()
