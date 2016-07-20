5f0716dc17490061bd2d5ea84fd2530f661e570a
Walter Holley III <holley.walter@gmail.com>
2016-07-19 17:18:16 -0700

    utility functions added -getInterestPayment -getRateperPeriod -convertInterest
    -fixed menu loop issues. no longer loops infinitely
    -initial formatting of report display completed

--- CSC110/week4/proj4_walterHolley.py
+++ CSC110/week4/proj4_walterHolley.py
@@ -8,30 +8,34 @@ a potential buyer's loan payments
 
 #main entry point for program
 def main():
+
     loanAmount, interestRate = inputLoanData()
     totalPeriods = showMenu();
-
+  
     while totalPeriods:
         #determine payment
-        paymentAmount = payment(loanAmount, interestRate, totalPeriods)
+        paymentAmount = payment(loanAmount, getRatePerPeriod(interestRate), totalPeriods)
 
         #display report
-        
+        showReport(loanAmount, interestRate, totalPeriods, paymentAmount)
 
-        #Pause
+        #Pause for menu input
+        input("Press <Enter> to continue")
+        totalPeriods = showMenu()
         
         
 ##================UTILTITY FUNCTIONS===================##
 
-#returns the present value of the loan
-#loanBalance: current loan balance
-#periodsRemaining: number of interest/payment periods left in the loan
-#interestRate: rate of interest charged per period
-def getPresentValue(loanBalance, periodsRemaining, interestRate):
-    return loanBalance / (1 + interestRate)**periodsRemaining
+def getInterestPayment(totalPayment, interestRate):
+    return totalPayment * interestRate
 
 
+def getRatePerPeriod(annualInterest):
+    return annualInterest / 12
 
+#converts received interest value to percentage
+def convertInterest(annualInterest):
+    return annualInterest * 0.01
 
 ##================OPERATIONS FUNCTIONS=================##
 
@@ -41,7 +45,7 @@ def inputLoanData():
     loanAmount = float(input("Enter loan amount, example 10000:"))
     interestRate = float(input("Enter annual interest rate, example 2.9:"))
 
-    return loanAmount, interestRate
+    return loanAmount, convertInterest(interestRate)
 
 def showMenu():
     choice = -1
@@ -54,24 +58,50 @@ def showMenu():
 
 
     #menu contents
-    for i in range(0,6):
+    for i in range(1,6):
         print("{}.{:<2}{}-month loan".format(i, " ", i * 12))
 
     print("{}.{:<2}EXIT".format(0, " "))
 
 
     #menu selection    
-    while choice < 0 and choice > 5:
+    while choice < 0 or choice > 5:
         choice = int(input("Choice:"))
 
     return choice * 12
 
-
+#Calculates the base principal payment for the loan
 def payment(presentValue, ratePerPeriod, numberOfPeriods):
-    return (ratePerPeriod * presentValue) / (1 - (1 + ratePerPeriod)**-numberOfPeriods)
+    return round((ratePerPeriod * presentValue) / (1 - (1 + ratePerPeriod)**-numberOfPeriods), 2)
 
 
-def showReport(PV, interestRate, numberOfPeriods, paymentAmount):
+def showReport(presentValue, annualInterestRate, numberOfPeriods, paymentAmount):
+    interestRate = getRatePerPeriod(annualInterestRate)
+    totalPaymentAmount = 0
+
+    print(paymentAmount)
+    #print report header
+    print("{:<8}{:>9}{:>10}{:>11}{:>12}".format("Pmt#", "PmtAmt", "Int", "Princ", "Balance"))
+    print("{:<8}{}{:>10}{:>11}{:>12}".format("-" * 4, "-" * 9, "-" * 5, "-" * 6, "-" * 8))
+
+    
+    for i in range(1, numberOfPeriods + 1):
+
+        #determine payments
+        interestPayment = presentValue * interestRate
+        principalPayment = paymentAmount - interestPayment
+        #totalPaymentAmount += principalPayment + interestPayment
+        totalPaymentAmount += paymentAmount
+        
+        #reduce balances and periods
+        presentValue -= principalPayment
+            
+        #display payments
+        print("{:>4}{:>13,.2f}{:>10.2f}{:>11.2f}{:>11.2f}".format(i, paymentAmount, interestPayment, principalPayment, presentValue))
+
+    print(totalPaymentAmount)
+
+        
     return 0
     
 
