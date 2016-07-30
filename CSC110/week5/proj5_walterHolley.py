d0ea247c2f8335dca264a0aa6c7d4c7eb3df5920
Walter Holley III <holley.walter@gmail.com>
2016-07-29 20:28:52 -0700

    proj5 -main execution updated to block run when file is imported -comments added -summary and testing notes added
    proj5_tests initial commit

--- CSC110/week5/proj5_walterHolley.py
+++ CSC110/week5/proj5_walterHolley.py
@@ -22,7 +22,7 @@ def main():
 ##================UTILITY FUNCTIONS====================##
 
 
-
+#writes formatted inventory line
 def printInventoryLine(itemDescription, itemValue):
     print("{:<15}{}".format(itemDescription, itemValue))
 
@@ -139,4 +139,45 @@ def fixEndYear(yearString):
 
 ##================MAIN EXECUTION=======================##
 
-main()
+#blocks main execution when imported by other files
+if __name__ == "__main__":
+    main()
+
+
+
+#TESTING
+#See proj5_tests_walterHolley.py
+'''
+I worked on a test harness for this project. You'll see the
+results inside of the above mentioned file.  I know it's not required,
+but I decided to give it a shot by writing code that executed the
+'fix' functions, and displayed what I thought the result should be, versus
+what was actually produced.  This helped me pick up on a few issues
+with some of the string slices I was doing earlier.
+'''
+
+#SUMMARY
+'''
+The approach for this was much like the other projects; implement
+what's known, and then fill in what isn't.  There wasn't much to
+really think about here, as most of the material was provided,
+all that needed to be done was implement the requirements for the
+fields.
+
+What I don't particularly like about this project is that it doesn't
+handle anything beyond the specific formatting of the inventory lines.
+There's plenty of room for bad data, and potentially many errors that can
+crash the program.
+
+The test harness is the bif take-away here.  It took a little bit of digging
+to understand how to get things working properly; especially to prevent execution
+of main for this program.
+Honorable mention goes to string slicing.  I haven't spent too much
+time with it, so it was good to get some practice; particularly around
+handling the start/end date and Part/description fields.
+
+For the next project, it looks like we'll be dealing with files.  I'm
+sure the string manipulation we did in this project will come in handy
+there, so I'll look to continue with string manipulation in that
+project.
+'''
