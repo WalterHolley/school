7d45d04cae461972bee1874c1ca96341f4f1adf0
Walter Holley III <holley.walter@gmail.com>
2017-01-09 12:51:29 -0800

    -Initial input validation added -Fixed rounding for EAN13 checksum validation -Header comments added

--- CSC143/Week1/WeeklyProblem2.java
+++ CSC143/Week1/WeeklyProblem2.java
@@ -1,67 +1,86 @@
+/*CSC143 - Walter Holley III
+* Weekly Problem 2
+* Write an application that determines if the provided code is a valid EAN 13
+* Barcode.
+* Barcode Information: https://en.wikipedia.org/wiki/International_Article_Number
+*/
+
 import java.util.Scanner;
 
 public class WeeklyProblem2 {
+
 	private static final String ERROR_MSG = "The code entered is not valid";
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		
+ 
 		Scanner console = new Scanner(System.in);
-		
+		System.out.println("To leave, type 'q' or 'quit'");
 		while(true){
-			int[] barcodeInput = new int[13];
-			int digitCount = 0;
+			String value = "";
 			System.out.println("Enter 13 digit numeric code");
-			// TODO change input ti string.  int is too small
-			while(console.hasNext()){
-				if(console.hasNextBigInteger()){
-					barcodeInput[digitCount] = scanner.nextInt();
-					digitCount++;
-					
-				
-				}
-				
+			value = console.nextLine();
+
+			if(value.toLowerCase().equals("q") || value.toLowerCase().equals("quit")){
+				break;
 			}
-			if(digitCount != 12){
+
+			if(!validateInput(value)){
+				System.out.println(ERROR_MSG);
+			}
+			else if(!validateEAN13(value)){
 				System.out.println(ERROR_MSG);
 			}
 			else{
-				if(!validateEAN13(barcodeInput)){
-					System.out.println(ERROR_MSG);
-				}
-				else{
-					System.out.println("The code entered is valid");
-				}
+				System.out.println("The code entered is valid");
 			}
 		}
-		
+		console.close();
+		System.out.println("Thanks for using this application.");
 
 	}
-	
+ 
+	//determines if the input received is valid
+	private static boolean validateInput(String input){
+		boolean isValid = false;
+		try{
+			if(input.length() != 13){
+				throw new NumberFormatException();
+			}	
+			for(int i = 0; i < 13; i++){
+				int value = Integer.parseInt(input.split("")[i]);
+			}
+			isValid = true;
+		}
+		catch(NumberFormatException ex){
+			//nothing to handle, just skips
+			//changing return value to true
+		}
+		return isValid;
+
+	}
+
 	//determines if the input received is a valid EAN 13 bar code
-	private static boolean validateEAN13(int[] digits){
+	private static boolean validateEAN13(String digits){
 		boolean isValid = false;
 		int checkSum = 0;
-		
-		//Determine checksum 
-		for(int i = 0; i < 11; i++){					
+
+		//Determine checksum
+		for(int i = 0; i < 12; i++){
+			int value = Integer.parseInt(digits.split("")[i]);
 			if(i % 2 == 0){
-				checkSum += digits[i] * 1;
+				checkSum += value * 1;
 			}
 			else{
-				checkSum += digits[i] * 3;
+				checkSum += value * 3;
 			}
 		}
-		
+
 		//validate checkSum against check digit
-		int checkDigit = (int)Math.round(Math.ceil((double)checkSum) - checkSum);
-		if(checkDigit == digits[12]){
-			isValid = false;
+		double roundedCheckSum = Math.ceil(checkSum * Math.pow(10, -Math.floor(Math.log10(checkSum)))) / Math.pow(10, -Math.floor(Math.log10(checkSum)));
+		int checkDigit = (int)roundedCheckSum - checkSum;
+		if(checkDigit == Integer.parseInt(digits.split("")[12])){
+			isValid = true;
 		}
-		
 		return isValid;
 	}
-
-
 }
