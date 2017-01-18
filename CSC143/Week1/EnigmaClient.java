a63297dc042f9345db06894f3e8f8b26a1cdee61
Walter Holley III <holley.walter@gmail.com>
2017-01-17 23:11:41 -0800

    EnigmaClient -selection logic for enigma sub-menu added -validation for encryption input added -stub for program demo added(not implemented)

--- CSC143/Week1/EnigmaClient.java
+++ CSC143/Week1/EnigmaClient.java
@@ -62,7 +62,6 @@ public static final String ERROR_MSG_INVALID_INPUT = "Invalid input.  Please try
 		System.out.println("2. Decrypt");
 		System.out.println("3. Run Default Example");
 		System.out.println("4. Return to Main Menu");
-		System.out.println("Enter Selection:");
 	}
 	
 	private static void processMainMenuSelection(int selection, Scanner console){
@@ -71,8 +70,8 @@ public static final String ERROR_MSG_INVALID_INPUT = "Invalid input.  Please try
 		if(selection == 1){
 			enigma = new Enigma();
 		}
+		//prepare custom enigma
 		else if(selection == 2){
-			//process custom rotors
 			boolean tryAgain = true;
 			while(tryAgain){
 				String innerRotor = "";
@@ -102,10 +101,94 @@ public static final String ERROR_MSG_INVALID_INPUT = "Invalid input.  Please try
 				
 			}
 		}
+		//show settings
 		else if(selection == 3){
 			System.out.printf("Inner Rotor: %s\nMiddle Rotor: %s\nOuter Rotor: %s\n", 
 					enigma.innerRotor, enigma.middleRotor, enigma.outerRotor);
 		}
+		
+		processEnigma(enigma, console);
+	}
+	
+	private static int getEnigmaMenuSelection(Scanner console){
+		showEnigmaMenu();
+		boolean isValidSelection = false;
+		int menuSelection = 0;
+		
+		while(!isValidSelection){
+			System.out.println("Enter Selection:");
+			try{	
+				menuSelection = console.nextInt();
+			}
+			catch(InputMismatchException e){
+				System.out.println(ERROR_MSG_INVALID_INPUT);
+				continue;
+			}
+			
+			if(menuSelection > 0 && menuSelection < 5){
+				isValidSelection = true;
+			}
+			else{
+				System.out.println(ERROR_MSG_INVALID_INPUT);
+			}
+		}
+		
+		return menuSelection;
+	}
+	
+	private static void processEnigma(Enigma enigma, Scanner console){
+		int selection = 0;
+		
+		while(selection != 4){
+			showEnigmaMenu();
+			selection = getEnigmaMenuSelection(console);
+			if(selection == 1){
+				System.out.println("Enter string to encrypt.  spaces and alpha characters only.");
+				String messageToEncrypt = console.nextLine();
+				if(!validateEncryptInput(messageToEncrypt)){
+					System.out.println(ERROR_MSG_INVALID_INPUT);
+				}
+				else{
+					System.out.printf("Encrypted message: %s\n", enigma.encrypt(messageToEncrypt));
+					enigma.reset();
+				}
+			}
+			else if(selection == 2){
+				System.out.println("Enter string to decrypt.  Ensure your rotor settings are correct.");
+				String messageToDecrypt = console.nextLine();
+				if(!validateEncryptInput(messageToDecrypt)){
+					System.out.println(ERROR_MSG_INVALID_INPUT);
+				}
+				else{
+					System.out.printf("Dencrypted message: %s\n", enigma.decrypt(messageToDecrypt));
+					enigma.reset();
+				}
+			}
+			else if(selection == 3){
+				runDemo();
+			}
+		}
+	}
+	
+	private static boolean validateEncryptInput(String message){
+		boolean isValid = true;
+		
+		for(int i = 0; i < message.length(); i++){
+			int x = (int)message.toUpperCase().toCharArray()[i];
+			
+			if(x != 32){
+				if(x < 65 && x > 90){
+					isValid = false;
+					break;
+				}
+			}
+		}
+		
+		return isValid;
+	}
+	
+	private static void runDemo(){
+		
 	}
 
 }
