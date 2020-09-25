67a72f346976354ddf853cde26b39491d3931740
Walter Holley III <holley.walter@gmail.com>
2020-09-24 23:47:59 -0500

    Search for directory process implemented.  handling of invalid directory added

--- UMSL/CMPSCI2750/project1/scnr.sh
+++ UMSL/CMPSCI2750/project1/scnr.sh
@@ -1,25 +1,35 @@
 bash
 #!/bin/bash
 
-echo "Scanner"
-echo $0
-
-function scanDir {
-
-  for directory in ls -F 
-  do
-    text="|-${directory}"
-    echo text
-    scanDirFiles directory $2
-  done
-
-
-}
-
-function scanDirFiles{
+function scanDirFiles {
   for file in find $1 -type f
     do
       text="|-${file}"
-      echo text
+      echo $text
     done
 }
+
+function scanDir {
+  echo "search dir: ${1}"
+  internalDirs=$(find $1 -type d)
+
+  for directory in $internalDirs
+  do
+    text="|-${directory}"
+    echo $text
+    scanDir directory
+  done
+}
+
+
+echo "Scanner"
+echo $1
+dirs=$(find $1 -type d | sort)
+echo $dirs
+
+if [[ ${dirs[@]} -eq 0 ]]
+then
+  echo "Directory not found"
+else
+  scanDir $dirs[0]
+fi
