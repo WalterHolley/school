6500e1564469457ab94b7ace5809513374fbcf0c
Walter Holley III <holley.walter@gmail.com>
2020-09-26 00:44:09 -0500

    tree search functionality completed.

--- UMSL/CMPSCI2750/project1/scnr.sh
+++ UMSL/CMPSCI2750/project1/scnr.sh
@@ -2,7 +2,9 @@ bash
 #!/bin/bash
 
 function scanDirFiles {
-  for file in find $1 -type f
+  echo "find Files in: $1"
+  files=$(find $1 -type f -maxdepth 1 | sort)
+  for file in $files
     do
       text="|-${file}"
       echo $text
@@ -10,26 +12,44 @@ function scanDirFiles {
 }
 
 function scanDir {
-  echo "search dir: ${1}"
-  internalDirs=$(find $1 -type d)
+  echo "search dir: $1"
+  local internalDirs=()
+  local offset=$(( $2 + 1 ))
 
-  for directory in $internalDirs
+  while IFS= read -r -d $'\0'
   do
-    text="|-${directory}"
-    echo $text
-    scanDir directory
-  done
+    internalDirs+=("$REPLY")
+  done < <(find $1 -type d -maxdepth 1 -print0| sort )
+  local size=${#internalDirs[@]}
+  echo "${internalDirs}"
+  echo $size
+
+  if [[ size -gt 0 ]]
+  then
+    internalDirs=$(find $1 -type d -maxdepth 1| sort| tail -$size)
+    echo "dir list: $internalDirs"
+    for directory in $internalDirs
+    do
+      if [[ $directory == $1 ]]
+      then
+        continue
+      fi
+      text=$(echo $directory | tr [$1] [""])
+      echo $text
+      scanDir $directory $offset
+    done
+  fi
+    scanDirFiles $1 $offset
 }
 
 
 echo "Scanner"
 echo $1
-dirs=$(find $1 -type d | sort)
-echo $dirs
+dirs=$(find $1 -type d -maxdepth 0| sort)
 
-if [[ ${dirs[@]} -eq 0 ]]
+if [[ -z $dirs ]]
 then
   echo "Directory not found"
 else
-  scanDir $dirs[0]
+  scanDir $dirs 0
 fi
