e693beb75536a5a72d1594bbd81c7bcaf0e92e9a
Walter Holley III <holley.walter@gmail.com>
2020-09-26 14:15:13 -0500

    text formatting for files and directory names

--- UMSL/CMPSCI2750/project1/scnr.sh
+++ UMSL/CMPSCI2750/project1/scnr.sh
@@ -2,17 +2,16 @@ bash
 #!/bin/bash
 
 function scanDirFiles {
-  echo "find Files in: $1"
   files=$(find $1 -type f -maxdepth 1 | sort)
   for file in $files
     do
-      text="|-${file}"
+      local fileName=$(echo $file | rev | cut -d'/' -f 1 | rev)
+      text="|-${fileName}"
       echo $text
     done
 }
 
 function scanDir {
-  echo "search dir: $1"
   local internalDirs=()
   local offset=$(( $2 + 1 ))
 
@@ -20,22 +19,20 @@ function scanDir {
   do
     internalDirs+=("$REPLY")
   done < <(find $1 -type d -maxdepth 1 -print0| sort )
+
   local size=${#internalDirs[@]}
-  echo "${internalDirs}"
-  echo $size
 
   if [[ size -gt 0 ]]
   then
     internalDirs=$(find $1 -type d -maxdepth 1| sort| tail -$size)
-    echo "dir list: $internalDirs"
     for directory in $internalDirs
     do
       if [[ $directory == $1 ]]
       then
         continue
       fi
-      text=$(echo $directory | tr [$1] [""])
-      echo $text
+      local dirName=$(echo $directory | rev | cut -d'/' -f 1 | rev)
+      echo $dirName
       scanDir $directory $offset
     done
   fi
@@ -43,13 +40,11 @@ function scanDir {
 }
 
 
-echo "Scanner"
-echo $1
 dirs=$(find $1 -type d -maxdepth 0| sort)
 
 if [[ -z $dirs ]]
 then
-  echo "Directory not found"
+  echo "$1: Directory not found"
 else
   scanDir $dirs 0
 fi
