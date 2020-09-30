fb2fc1e844d9b7e99fdd774650cb229db84efc36
Walter Holley III <holley.walter@gmail.com>
2020-09-30 00:15:02 -0500

    Formatting and text coloring completed.

--- UMSL/CMPSCI2750/project1/scnr.sh
+++ UMSL/CMPSCI2750/project1/scnr.sh
@@ -1,22 +1,17 @@
 bash
 #!/bin/bash
-
-function getOffset {
-  local offsetText=$(printf "|--"'%.0s' $(seq $1))
-  local space=" "
-
-  local result=${offsetText//-/"  "}
-  echo $result
-}
+YELLOW_COLOR='\033[1;33m'
+GREEN_COLOR='\033[0;32m'
+NO_COLOR='\033[0m'
 
 function scanDirFiles {
   files=$(find $1 -type f -maxdepth 1 | sort)
   for file in $files
     do
-      local fileName=$(echo $file | rev | cut -d'/' -f 1 | rev)
-      local dirSpacing=$(getOffset $2)
-      text="${dirSpacing}-${fileName}"
-      echo $text
+      #local fileName=$(echo $file | rev | cut -d'/' -f 1 | rev)
+      local dirSpacing=$(echo $file |sed -e "s/[^\/]*\//--/g")
+      #text="${YELLOW_COLOR}$${dirSpacing}"
+      printf "${GREEN_COLOR}${dirSpacing}${NO_COLOR}\n"
     done
 }
 
@@ -34,19 +29,22 @@ function scanDir {
   if [[ size -gt 0 ]]
   then
     internalDirs=$(find $1 -type d -maxdepth 1| sort| tail -$size)
+
     for directory in $internalDirs
     do
       if [[ $directory == $1 ]]
       then
+        scanDirFiles $1 $offset
         continue
       fi
-      local dirName=$(echo $directory | rev | cut -d'/' -f 1 | rev)
-      local dirSpacing=$(getOffset $2)
-      echo $dirSpacing-$dirName
+      #local dirName=$(echo $directory | rev | cut -d'/' -f 1 | rev)
+      local dirSpacing=$(echo $directory |sed -e "s/[^\/]*\//--/g")
+
+      printf "${YELLOW_COLOR}$dirSpacing${NO_COLOR}\n"
+
       scanDir $directory $offset
     done
   fi
-    scanDirFiles $1 $offset
 }
 
 
@@ -56,5 +54,6 @@ if [[ -z $dirs ]]
 then
   echo "$1: Directory not found"
 else
+  echo $dirs
   scanDir $dirs 0
 fi
