0776eb2b5b4fccff22ce2b1f7582069269a5300c
Walter Holley III <holley.walter@gmail.com>
2020-10-21 23:58:50 -0500

    Script updates
    -change directory function added
    -main menu selection updated.

--- UMSL/CMPSCI2750/project2/clnup.sh
+++ UMSL/CMPSCI2750/project2/clnup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+workingDirectory=$PWD
 
 function mainMenu {
+  printf "\033c"
   echo "***********"
   echo "* Cleanup *"
   echo "***********"
+  echo "Working Directory: $workingDirectory"
   echo "1>>Clear empty files and directories"
   echo "2>>Move Files"
   echo "3>>Compress files"
@@ -12,7 +15,41 @@ function mainMenu {
 
   read selection
 
-  echo "you entered $selection"
+  case $selection in
+    [1]*)
+    #clear Files
+    ;;
+    [2]*)
+    #move Files
+    ;;
+    [3]*)
+    #compress Files
+    ;;
+    [4]*)
+    changeDir
+    ;;
+    [5]*)
+    printf "\033c"
+    echo "Exiting tool."
+    exit 0
+    ;;
+  esac
+}
+
+function changeDir {
+  echo "Enter Directory:"
+  read selection
+  dirs=$( find $selection -maxdepth 0 -type d | sort )
+
+  if [[ -z $dirs ]]
+  then
+    mainMenu
+  else
+    workingDirectory=$dirs
+    cd $workingDirectory
+    echo "Working directory updated"
+  fi
+  mainMenu
 }
 
 mainMenu
