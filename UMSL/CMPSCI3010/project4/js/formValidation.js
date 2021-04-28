aa8d2a4dbc35a226cf426ad3c3128b97b85b8e7d
Walter Holley III <holley.walter@gmail.com>
2021-04-27 19:08:58 -0500

    JS Updates
    Frontend js cleanup and radio button validation bug fix

--- UMSL/CMPSCI3010/project4/js/formValidation.js
+++ UMSL/CMPSCI3010/project4/js/formValidation.js
@@ -1,7 +1,18 @@
 
-var fieldIDs = ["userName", "password", "confirmPassword", "firstName",
-"lastName", "maritalStatus", "gender", "dateOfBirth", "address1", "address2", "city", "state", "zipCode", "phone","email"];
+var regex = null;
+var requiredFieldIDs = ["userName", "password", "confirmPassword", "firstName",
+"lastName", "dateOfBirth", "address1", "city", "state", "zipCode", "phone","email"];
 
+var optionalFieldIDs = ["address2"];
+
+function validateContentSize(element, minSize, maxSize){
+
+  var result = false;
+  if(element.value.length <= maxSize && element.value.length >= minSize)
+    result = true;
+
+  return result;
+}
 
 function resetElements(){
   //reeset required fields
@@ -9,10 +20,12 @@ function resetElements(){
   gender = document.getElementsByName("gender");
   maritalStatus = document.getElementsByName("maritalStatus");
 
+  //disable Submit
+  document.getElementById("submit").disabled = true;
 
   //clear required fields
-  for(i = 0; i < fieldIDs.length; i++){
-    element = document.getElementById(fieldIDs[i]);
+  for(i = 0; i < requiredFieldIDs.length; i++){
+    element = document.getElementById(requiredFieldIDs[i]);
     element.value = '';
     element.classList.remove("is-valid");
     element.classList.remove("is-invalid");
@@ -29,44 +42,126 @@ function resetElements(){
   }
 
 }
+//Ensures a state value is selected
+function validateState(){
+  result = false;
+  stateList = document.getElementById("state");
 
-function markRadio(elementName){
-  var element = document.getElementsByName(elementName);
-  var isChecked = false;
+  if(stateList.options[stateList.selectedIndex].value !== null ||
+    !stateList.options[stateList.selectedIndex].value === ""){
+    result = true;
+  }
+  return result;
+}
 
-  for(var i = 0; i < element.length; i++){
-    if(element[i].checked){
-      isChecked = true;
-      break;
+//validate phone number
+function validatePhone(element){
+  result = false;
+  let phoneRegex1 = /[0-9]{10}/;
+  let phoneRegex2 = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+  length = element.value.length;
+
+
+if(length == 10 || length == 12){
+  regex = new RegExp(phoneRegex1);
+  if(regex.test(element.value)){
+    result = true;
+    element.value = element.value.slice(0,3) + "-" + element.value.slice(3,6) + "-" + element.value.slice(6,11);
+  }else{
+    regex = new RegExp(phoneRegex2);
+    if(regex.test(element.value)){
+      result = true;
+      element.value = element.value.substring(0,12);
     }
-  }
-
-  if(isChecked){
-    hideElement(elementName + "Error");
-  }
-  else{
-    showElement(elementName + "Error");
   }
 }
 
+  return result;
 
-function showElement(elementName){
-  var element = document.getElementById(elementName);
+}
 
-  if(element.classList.contains("hide")){
-    element.classList.remove("hide");
+function validateZipCode(element){
+  result = false;
+  let zipCodeRegex = /(^\d{5}-\d{4})|(^\d{9})|(^\d{5})/;
+  let zipCodeRegexNineDigit = /(^\d{9})/;
+
+  length = element.value.length;
+  regex = new RegExp(zipCodeRegex);
+
+  if(regex.test(element.value)){
+
+    if(length == 10 || length == 5){
+      result = true;
+    }else if(length == 9 && zipCodeRegexNineDigit.test(element.value)){
+      result = true;
+      element.value = element.value.slice(0,5) + "-" + element.value.slice(5,9);
+    }
+
   }
 
+  return result;
+}
+
+function validatePassword(passwordValue){
+  result = false;
+  let lowercase = /[a-z]/;
+  let upperCase = /[A-Z]/;
+  let digit = /[0-9]/;
+  let special = /[^a-zA-Z0-9]/;
+
+  regex = new RegExp(lowercase);
+    //contains one lowercase
+  if(regex.test(passwordValue)){
+    regex = new RegExp(special);
+    //contains one special
+    if(regex.test(passwordValue)){
+      regex = new RegExp(upperCase);
+      //contains one uppercase
+      if(regex.test(passwordValue)){
+        regex = new RegExp(digit);
+        //contains one digit
+        if(regex.test(passwordValue)){
+          result = true;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+function validateEmail(element){
+  let emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  result = false;
+
+  if(emailRegex.test(element.value)){
+    result = true;
+  }
+
+  return result;
+}
+
+function validateDOB(element){
+  result = false;
+  let dateRegex = /(^\d{4}-\d{2}-\d{2})/;
+  var currentDate = new Date();
+
+  if(dateRegex.test(element.value) && element.value.length == 10){
+    var selectedDate = new Date(element.value);
+    if(selectedDate < currentDate){
+      result = true;
+    }
+  }
+  return result;
+}
+
+function showElement(elementName){
+  document.getElementById(elementName).classList.remove("hide");
 
 }
 
 function hideElement(elementName){
-  var element = document.getElementById(elementName);
-
-  if(!element.classList.contains("hide")){
-    element.classList.add("hide");
-  }
-
+  document.getElementById(elementName).classList.add("hide");
 
 }
 
@@ -85,6 +180,81 @@ function markInvalidElement(element){
   //remove invalid class
   element.classList.add("is-invalid");
 
+}
+
+//determines of the radio buttons have been selected,
+//manages their error states
+function validateRadioButtons(){
+  var result = false;
+  gender = document.getElementsByName("gender");
+  maritalStatus = document.getElementsByName("maritalStatus");
+  genderError = document.getElementById("genderError");
+  statusError = document.getElementById("statusError");
+  genderSelected = false;
+  statusSelected = false;
+  var i;
+
+  for(i = 0; i < gender.length; i++){
+    if(gender[i].checked){
+      genderSelected = true;
+      break;
+    }
+  }
+
+  for(i = 0; i < maritalStatus.length; i++){
+    if(maritalStatus[i].checked){
+      statusSelected = true;
+      break;
+    }
+  }
+
+
+
+  if(!genderSelected){
+    showElement("genderError");
+  }else{
+    hideElement("genderError");
+  }
+
+  if(!statusSelected){
+    showElement("statusError");
+  }else{
+    hideElement("statusError");
+  }
+
+  if(genderSelected && statusSelected){
+    result = true;
+  }
+
+  return result;
+}
+
+//determines of a radio button has a selected option
+function validateRadioButton(elementCollection){
+  var result = false;
+
+  for(i = 0; i < elementCollection.length; i++){
+    if(elementCollection[i].checked){
+      result = true;
+      break;
+    }
+  }
+
+  return result;
+}
+
+//validates the form
+function formIsValid(){
+  result = false;
+  //check required and optional fields
+  if(validateRequiredFields() && !document.getElementById("address2").classList.contains("is-invalid")){
+    result = true;
+    document.getElementById("submit").disabled = false;
+  }else{
+    document.getElementById("submit").disabled = true;
+  }
+
+  return result;
 }
 
 //Updates the error states on each item
@@ -115,6 +285,37 @@ function updateErrors(errorList){
 
 }
 
+//determines if all required fields have been properly populated
+function validateRequiredFields(){
+  var result = true;
+  gender = document.getElementsByName("gender");
+  maritalStatus = document.getElementsByName("maritalStatus");
+  var i;
+  for(i = 0; i < requiredFieldIDs.length; i++){
+    if(!document.getElementById(requiredFieldIDs[i]).classList.contains("is-valid")){
+      result = false;
+    }
+  }
+
+  if(result){
+    result = validateRadioButton(gender);
+    result = validateRadioButton(maritalStatus);
+  }
+
+  return result;
+}
+
+function validateOptionalFields(){
+  var i;
+  result = true;
+
+  for(i = 0; i < optionalFieldIDs.length; i++){
+    if(document.getElementById(optionalFieldIDs[i]).classList.contains("is-invalid")){
+      result = false;
+    }
+  }
+}
+
 //Submit form
 async function sendForm(){
   var form = document.getElementById("registrationForm");
@@ -136,11 +337,207 @@ async function sendForm(){
 
 }
 
+//initialize form
 function init(){
+  //disable Submit
+  document.getElementById("submit").disabled = true;
+  var i;
+  maritalStatus = document.getElementsByName("maritalStatus");
+  gender = document.getElementsByName("gender");
+
+
+  document.getElementById("userName").addEventListener("change", function(){
+      var element = document.getElementById("userName");
+      if(!validateContentSize(element,6, 50)){
+        markInvalidElement(element);
+        showElement("userNameError");
+      }else{
+        markValidElement(element);
+        hideElement("userNameError");
+      }
+      formIsValid();
+  });
+
+  document.getElementById("password").addEventListener("change", function(){
+    var element = document.getElementById("password");
+    if(!validateContentSize(element,8, 50) || !validatePassword(element.value)){
+      markInvalidElement(element);
+      showElement("passwordError");
+    }else{
+      markValidElement(element);
+      hideElement("passwordError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("confirmPassword").addEventListener("change", function(){
+    var element = document.getElementById("confirmPassword");
+    originalPassword = document.getElementById("password").value;
+    if(!validateContentSize(element, 8, 50) || !validatePassword(element.value) ||
+     element.value !== originalPassword){
+      markInvalidElement(element);
+      showElement("confirmPasswordError");
+    }else{
+      markValidElement(element);
+      hideElement("confirmPasswordError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("firstName").addEventListener("change", function(){
+    var element = document.getElementById("firstName");
+    if(!validateContentSize(element, 1, 50)){
+      markInvalidElement(element);
+      showElement("firstNameError");
+    }else{
+      markValidElement(element);
+      hideElement("firstNameError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("lastName").addEventListener("change", function(){
+    var element = document.getElementById("lastName");
+    if(!validateContentSize(element, 1, 50)){
+      markInvalidElement(element);
+      showElement("lastNameError");
+    }else{
+      markValidElement(element);
+      hideElement("lastNameError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("address1").addEventListener("change", function(){
+    var element = document.getElementById("address1");
+    if(!validateContentSize(element, 1, 100)){
+      markInvalidElement(element);
+      showElement("address1Error");
+    }else{
+      markValidElement(element);
+      hideElement("address1Error");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("address2").addEventListener("change", function(){
+    var element = document.getElementById("address2");
+    if(!validateContentSize(element, 1, 100)){
+      markInvalidElement(element);
+      showElement("address2Error");
+    }else{
+      markValidElement(element);
+      hideElement("address2Error");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("city").addEventListener("change", function(){
+    var element = document.getElementById("city");
+    if(!validateContentSize(element, 1, 50)){
+      markInvalidElement(element);
+      showElement("cityError");
+    }else{
+      markValidElement(element);
+      hideElement("cityError");
+    }
+      formIsValid();
+  });
+
+  document.getElementById("state").addEventListener("change", function(){
+    var element = document.getElementById("state");
+    if(!validateState()){
+      markInvalidElement(element);
+      showElement("stateError");
+    }else{
+      markValidElement(element);
+      hideElement("stateError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("zipCode").addEventListener("change", function(){
+    var element = document.getElementById("zipCode");
+    if(!validateZipCode(element)){
+      markInvalidElement(element);
+      showElement("zipCodeError");
+    }else{
+      markValidElement(element);
+      hideElement("zipCodeError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("phone").addEventListener("change", function(){
+    var element = document.getElementById("phone");
+    if(!validatePhone(element)){
+      markInvalidElement(element);
+      showElement("phoneError");
+    }else{
+      markValidElement(element);
+      hideElement("phoneError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("email").addEventListener("change", function(){
+    var element = document.getElementById("email");
+    if(!validateEmail(element)){
+      markInvalidElement(element);
+      showElement("emailError");
+    }else{
+      markValidElement(element);
+      hideElement("emailError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("dateOfBirth").addEventListener("change", function(){
+    var element = document.getElementById("dateOfBirth");
+    if(!validateDOB(element)){
+      markInvalidElement(element);
+      showElement("dateOfBirthError");
+    }else{
+      markValidElement(element);
+      hideElement("dateOfBirthError");
+    }
+    formIsValid();
+  });
+
+  document.getElementById("reset").addEventListener("click", function(){
+      resetElements();
+  });
+
   document.getElementById("submit").addEventListener("click", function(){
-      sendForm();
+      if(formIsValid()){
+        sendForm();
+      }
 
   });
+
+  //event listeners for radio buttons
+  for(i = 0; i < gender.length; i++){
+    gender[i].addEventListener("change", function(){
+      if(!validateRadioButton(gender)){
+        showElement("genderError");
+      }else{
+        hideElement("genderError");
+      }
+    });
+  }
+
+  for(i = 0; i < maritalStatus.length; i++){
+    maritalStatus[i].addEventListener("change", function(){
+      if(!validateRadioButton(maritalStatus)){
+        showElement("maritalStatusError");
+        formIsValid()
+      }else{
+        hideElement("maritalStatusError");
+        formIsValid();
+      }
+    });
+  }
+
 }
 
 init();
