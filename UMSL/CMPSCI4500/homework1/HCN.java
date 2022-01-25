6ad94e0a08660f980ab9e2e27971c30d2ebadaad
Walter Holley III <holley.walter@gmail.com>
2022-01-24 23:34:36 -0600

    Functions added for prime numbers, exponent retrieval, and prime factors.

--- UMSL/CMPSCI4500/homework1/HCN.java
+++ UMSL/CMPSCI4500/homework1/HCN.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class HCN {
     /**
      * CMP SCI 4500
@@ -11,11 +14,20 @@ public class HCN {
      */
     // Max integer limit for application
     private static final int NUM_LIMIT = 10000000;
-
+    private static ArrayList<Integer> primeList = new ArrayList<>();
+    private static HashMap<Integer, Integer> HCN_LIST;
     private static final String SELECTION_ERROR = "You've made an invalid selection.  Try again.";
 
     public static void main(String[] args) {
+        init();
         menu();
+    }
+
+    /**
+     * Setup application prior to execution
+     */
+    private static void init() {
+        HCN_LIST = getHCNList(NUM_LIMIT);
     }
 
     /**
@@ -72,6 +84,66 @@ public class HCN {
             }
 
         }
+    }
+
+    /**
+     * Determines if the given number is a prime number
+     * 
+     * @param number The number to check for prime
+     * @return booelan true if prime, otherwise false
+     */
+    private static boolean isPrime(int number) {
+        boolean result = false;
+
+        if (number > 1) {
+            if (number == 2)
+                result = true;
+            else {
+                for (int i = 2; i < number; i++) {
+
+                    if (number % i == 0)
+                        break;
+                    else if (i == number - 1)
+                        result = true;
+
+                }
+            }
+
+        }
+        return result;
+    }
+
+    /**
+     * Generates a hashmap containing Highly Composite Numbers
+     * 
+     * @param numLimit the max value to evaluate(this value will be evaluated as
+     *                 well)
+     * @return Hashmap of HCNs. Key is the HCN, value is the divisor
+     */
+    private static HashMap<Integer, Integer> getHCNList(int numLimit) {
+        HashMap<Integer, Integer> hcnList = new HashMap<Integer, Integer>();
+
+        for (int i = 1; i <= numLimit; i++) {
+
+            // check if value is a prime number
+            if (isPrime(i))
+                primeList.add(i);
+
+            // find factors of value
+        }
+
+        return hcnList;
+    }
+
+    private static int[] getPrimeFactors(int number) {
+
+        int maxExp = primeList.size();
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+    }
+
+    private static int getExponent(int target, int maxExponent, int prime) {
+
     }
 
 }
