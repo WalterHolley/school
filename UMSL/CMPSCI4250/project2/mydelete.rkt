288b35813c81657c8026b5a69731e6cd6888917d
Walter Holley III <holley.walter@gmail.com>
2022-02-27 00:44:58 -0800

    Prject 2 part 4 update:  working test 1

--- UMSL/CMPSCI4250/project2/mydelete.rkt
+++ UMSL/CMPSCI4250/project2/mydelete.rkt
@@ -1,12 +1,18 @@
 #lang racket
 
-(define (my_delete atm list1)(
+(define (my_delete atm list1)
     (cond
         [(null? atm) list1]
-        [(null? list1) list1]
-        [else cons(remove (list atm) cdr(list1))]
+        [(null? list1) '()]
+        ;[(list? (car list1)) (cons (my_delete atm (car list1)) (cdr list1))]
+        [(not (equal? atm (car list1))) (cons (car list1) (my_delete atm (cdr list1)))]
+        [else cons car list1 (my_delete atm (cdr list1))]
     )
-))
+)
 
-(my_delete 1 (list 1 2 3 4 5 6 2)) ;should return (2 3 4 5 6 2)
-(my_delete '2 (list 1 2 3 4 5 6 2)) ;should return (1 3 4 5 6)
+(define testList1 (list 1 2 3 4 5 1 6 2))
+(define testList2 (list 3 2 1 (list 3 2) 2 6 5 4))
+
+(my_delete 1 testList1) ;should return (2 3 4 5 6 2)
+
+(my_delete 2 testList2) ;should return (3 1 (3) 6 5 4)
