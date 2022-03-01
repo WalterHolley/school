39be2bea933cde7506b68fcd079be5b8e2c5e982
Walter Holley III <holley.walter@gmail.com>
2022-02-28 23:08:40 -0600

    Project 2 part1 testing

--- UMSL/CMPSCI4250/project2/mycalc.rkt
+++ UMSL/CMPSCI4250/project2/mycalc.rkt
@@ -1,25 +1,40 @@
 #lang racket
 (define pi 3.1416)
 
-(define (my_calc calc radius)
-    (if(= 1 calc)
-        (* pi (* radius radius))
-        (* (/ 4 3) (* pi (expt radius 3)))
-    )
-)
-
-(define (my_calc_cond calc radius)
+;part a multiple path implementation
+(define (my_calc_a calc radius)
     (cond
-        [(null? calc) null]
+        [(or (null? calc) (< radius 0) (<= calc 0) (> calc 2)) #f]
         [(= calc 1) (* pi (* radius radius))]
         [(= calc 2) (* (/ 4 3) (* pi (expt radius 3)))]
-        [(> calc 2) null]
     )
 )
 
-(my_calc 1 23) ;should return roughly 1661.9
-(my_calc 2 23) ;should return roughly 50965.01
-(my_calc_cond 3 23) ;should return null
-(my_calc_cond 1 23) ;should return roughly 1661.9
-(my_calc_cond 2 23) ;should return roughly 50965.01
-(my_calc_cond null 23) ;should return null
+; part b two-way implementation
+(define (my_calc_b calc radius)
+    (if (> radius 0)
+        (if (null? calc) #f
+           (if(= 1 calc)
+                (* pi (* radius radius))
+                (if(= 2 calc)
+                    (* (/ 4 3) (* pi (expt radius 3)))
+                    #f
+                )
+            ) 
+        )
+        #f
+    )
+)
+
+
+
+(my_calc_a 1 23) ;should return roughly 1661.9
+(my_calc_a 2 23) ;should return roughly 50965.01
+(my_calc_a 3 23) ;should return #f
+(my_calc_a 2 -23); should return #f
+(my_calc_a null 23) ;should return #f
+(my_calc_b 3 23) ;should return #f
+(my_calc_b 1 23) ;should return roughly 1661.9
+(my_calc_b 2 23) ;should return roughly 50965.01
+(my_calc_b null 23) ;should return #f
+(my_calc_b 2 -23) ; should return #f
