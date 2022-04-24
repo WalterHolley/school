2dc692e0ef43ebf515e14b142375a1227e300fc7
Walter Holley III <holley.walter@gmail.com>
2022-04-23 22:27:14 -0500

    Project 4 Second function completed

--- UMSL/CMPSCI4250/project4/main.c
+++ UMSL/CMPSCI4250/project4/main.c
@@ -3,7 +3,8 @@
 
 
 int main(){
-    f1();
+    //f1();
+    f2();
     return 0;
 }
 
@@ -15,6 +16,7 @@ void f1(){
 
     n++;
 
+    //TODO: review ARI calculations
     if(n <= 10){
         printf("Call #%d at %x\n", n, &test);
         printf("AR Size #%d is %d \n\n", n, addr - (long)&test);
@@ -23,4 +25,20 @@ void f1(){
     }
     
     
+}
+
+void f2(){
+    
+    static int n = 0;
+    char* test[DEFAULT_SIZE];
+    static long int addr;
+
+    n++;
+
+    //TODO: review ARI calculations
+        printf("Call #%d at %x\n", n, &test);
+        printf("AR Size #%d is %d \n", n, addr - (long)&test);
+        printf("Stack Size #%d is %d \n\n", n, n * (addr - (long)&test) );
+        addr = &test;
+        f2();    
 }
