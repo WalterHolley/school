a8ab4cb9b3f0ddf3131873b9818fdc55ae0acae0
Walter Holley III <holley.walter@gmail.com>
2022-04-24 11:16:43 -0500

    Project 4 updates third function requirement added

--- UMSL/CMPSCI4250/project4/main.c
+++ UMSL/CMPSCI4250/project4/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#define DEFAULT_SIZE 1000
+#include <stdlib.h>
 
+#define DEFAULT_SIZE 1000
+#define ALLOC_ARR(size)((char*) malloc(size))
 
 int main(){
     //f1();
-    f2();
+    //f2();
+    f3();
     return 0;
 }
 
@@ -41,4 +44,26 @@ void f2(){
         printf("Stack Size #%d is %d \n\n", n, n * (addr - (long)&test) );
         addr = &test;
         f2();    
+}
+
+void f3(){
+    static int n = 0;
+    //allocate array
+    char* test = ALLOC_ARR(DEFAULT_SIZE);
+    char * currentAddr = (char*)&test;
+    static long int addr;
+
+    n++;
+
+    if(n <= 10){
+        printf("Call #%d at %x\n", n, &test);
+        printf("AR Size #%d is %d \n\n", n, addr - (long int)currentAddr);
+        //free array
+        free(test);
+        addr = (long)currentAddr;
+        f3();
+    }
+
+    if(test)
+        free(test); 
 }
