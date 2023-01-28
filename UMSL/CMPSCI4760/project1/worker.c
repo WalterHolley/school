96d2a5cacc324d1348e2c5d7df6a22b096f39118
Walter Holley III <holley.walter@gmail.com>
2023-01-27 18:14:41 -0600

    Project 1: worker implementation

--- UMSL/CMPSCI4760/project1/worker.c
+++ UMSL/CMPSCI4760/project1/worker.c
@@ -1,15 +1,35 @@
 
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc char* argv[])
+void printWorkerInfo(int iteration, int pid, int ppid)
 {
-    if(argc > 0)
-    {
+    printf("WORKER PID:%s PPID:%s Iteration:%n ");
+}
 
+int main(int argc, char* argv[])
+{
+    int iterations;
+
+    if(argc > 1)
+    {
+        iterations = atoi(argv[1]);
+        int pid = getpid();
+        int ppid = getppid();
+        int i;
+        for(i = 0; i < iterations; i++)
+        {
+            printWorkerInfo(i + 1, pid, ppid);
+            printf("before sleeping\n");
+            sleep(1);
+            printWorkerInfo(i + 1, pid, ppid);
+            printf("after sleeping\n");
+
+        }
     }
     else
     {
-
+        printf("No argument for worker\n");
     }
     return 0;
 }
