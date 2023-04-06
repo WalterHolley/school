8be9d48fdc194ec75581a95a22518b7defb210c7
Walter Holley III <holley.walter@gmail.com>
2023-04-05 22:10:27 -0500

    Project 2 API for TMDB added

--- UMSL/CMPSCI4020/prj2/app/src/main/java/com/umsl/wdhq58/prj2/MainActivity.kt
+++ UMSL/CMPSCI4020/prj2/app/src/main/java/com/umsl/wdhq58/prj2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.umsl.wdhq58.prj2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.umsl.wdhq58.prj2.utility.api.ServiceBuilder
+import com.umsl.wdhq58.prj2.utility.api.TMDBService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val service = ServiceBuilder.buildService(TMDBService::class.java)
     }
 }
