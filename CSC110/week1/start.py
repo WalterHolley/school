8f17ddaf06e4af18b433d2c859886d301538cc6c
Walter Holley III <holley.walter@gmail.com>
2025-02-09 13:53:07 -0600

    Re-organization of school repo

--- CSC110/week1/start.py
+++
@@ -1,8 +0,0 @@
-# CSC 110 Orientation - Walter Holley III
-# Program that prints message to the user
-
-print ("Hello, World!")
-print ("My name is Walter")
-print ("The study of Computer Science continues!")
-print ("Saint Louis, MO is a good place to be from.")
-print ("This\nis \nformatting!")
